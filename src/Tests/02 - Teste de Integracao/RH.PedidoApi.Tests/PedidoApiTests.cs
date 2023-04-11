acc761433c72f8257e90b9c2e15ba3fcce545db5
Alex Jussiani Junior <alexjunior.jussiani@gmail.com>
2023-04-10 23:33:41 -0300

    Criação do teste de integração para atualizar um item

--- src/Tests/02 - Teste de Integracao/RH.PedidoApi.Tests/PedidoApiTests.cs
+++ src/Tests/02 - Teste de Integracao/RH.PedidoApi.Tests/PedidoApiTests.cs
@@ -20,15 +20,15 @@ namespace RH.Integration.Tests
             _testsFixture = testsFixture;
         }
 
-        [Fact(DisplayName = "Adicionar item em novo pedido")]
+        [Fact(DisplayName = "Adicionar item em novo pedido"), TestPriority(1)]
         [Trait("Categoria", "Integração API - Pedido")]
         public async Task AdicionarItem_NovoPedido_DeveRetornarComSucesso()
         {
             var item = new ItemViewModel
             {
 
-                ProdutoId = Guid.NewGuid(),
-                PedidoId = Guid.NewGuid(),
+                ProdutoId = new Guid("576bc18a-6617-4d99-8bd6-f00fa35c0ee0"),
+                PedidoId = new Guid("bd32ca50-7023-4760-9630-dfa7d07f9657"),
                 ProdutoNome = "X-tudo",
                 Quantidade = 2,
                 ValorUnitario = 22
@@ -40,5 +40,46 @@ namespace RH.Integration.Tests
             // Assert
             postResponse.EnsureSuccessStatusCode();
         }
+
+        [Fact(DisplayName = "Editar item em novo pedido"), TestPriority(2)]
+        [Trait("Categoria", "Integração API - Pedido")]
+        public async Task EditarItem_NovoPedido_DeveRetornarComSucesso()
+        {
+            var item = new ItemViewModel
+            {
+
+                ProdutoId = new Guid("576BC18A-6617-4D99-8BD6-F00FA35C0EE0"),
+                PedidoId = new Guid("F12B7756-4485-4E28-98BF-F4B26C6CF53B"),
+                ProdutoNome = "X-tudo",
+                Quantidade = 3
+            };
+
+            // Act
+            var postResponse = await _testsFixture.Client.PutAsJsonAsync("api/pedido", item);
+
+            // Assert
+            postResponse.EnsureSuccessStatusCode();
+        }
+
+        //[Fact(DisplayName = "Remover item em novo pedido")]
+        //[Trait("Categoria", "Integração API - Pedido")]
+        //public async Task RemoverItem_NovoPedido_DeveRetornarComSucesso()
+        //{
+        //    var item = new ItemViewModel
+        //    {
+
+        //        ProdutoId = Guid.NewGuid(),
+        //        PedidoId = Guid.NewGuid(),
+        //        ProdutoNome = "X-tudo",
+        //        Quantidade = 2,
+        //        ValorUnitario = 22
+        //    };
+
+        //    // Act
+        //    var postResponse = await _testsFixture.Client.DeleteAsync("api/pedido", item);
+
+        //    // Assert
+        //    postResponse.EnsureSuccessStatusCode();
+        //}
     }
 }
