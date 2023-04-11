b2971c61ada8eb86791e1bf1e5d327d9b55f2f18
Alex Jussiani Junior <alexjunior.jussiani@gmail.com>
2023-04-10 23:12:54 -0300

    1 - Criação do ambiente de testes de integração 2 - Criação do teste de integração para cadastrar um novo item a um pedido vazio

---
+++ src/Tests/02 - Teste de Integracao/RH.PedidoApi.Tests/PedidoApiTests.cs
@@ -0,0 +1,44 @@
+﻿using RH.Integration.Tests.Config;
+using RH.Pedidos.API;
+using RH.Pedidos.API.Application.Queries.ViewModels;
+using System;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RH.Integration.Tests
+{
+    [TestCaseOrderer("Features.Tests.PriorityOrderer", "Features.Tests")]
+    [Collection(nameof(IntegrationApiTestsFixtureCollection))]
+    public class PedidoApiTests
+    {
+
+        private readonly IntegrationTestsFixture<Startup> _testsFixture;
+
+        public PedidoApiTests(IntegrationTestsFixture<Startup> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar item em novo pedido")]
+        [Trait("Categoria", "Integração API - Pedido")]
+        public async Task AdicionarItem_NovoPedido_DeveRetornarComSucesso()
+        {
+            var item = new ItemViewModel
+            {
+
+                ProdutoId = Guid.NewGuid(),
+                PedidoId = Guid.NewGuid(),
+                ProdutoNome = "X-tudo",
+                Quantidade = 2,
+                ValorUnitario = 22
+            };
+
+            // Act
+            var postResponse = await _testsFixture.Client.PostAsJsonAsync("api/pedido", item);
+
+            // Assert
+            postResponse.EnsureSuccessStatusCode();
+        }
+    }
+}
